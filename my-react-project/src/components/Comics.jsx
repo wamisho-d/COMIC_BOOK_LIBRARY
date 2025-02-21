8fef9b12842ddfc7b9ae2ffb26810a56c013a3d7
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-21 12:20:00 -0500

    Update Comics.jsx

---
+++ my-react-project/src/components/Comics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Comics = () => {
+  return (
+    <div>
+      <h2>Comics</h2>
+      <p>This component is under construction. Check back later!</p>
+    </div>
+  );
+};
+
+export default Comics;
